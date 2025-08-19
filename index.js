fc323e1042e515a10c5f8533d0971d9ba24fff1f
NagarajuTJ <nagarajfindme@gmail.com>
2025-08-20 01:03:40 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/",(req, res) =>{
+    res.render("index.ejs", {
+  title: "Home",
+  activePage: "home"
+});
+});
+
+app.get("/about",(req, res) =>{
+    res.render("about.ejs", {
+  title: "About",
+  activePage: "About"
+});
+});
+
+app.get("/blog",(req, res) =>{
+    res.render("blog.ejs", {
+  title: "Blog",
+  activePage: "Blog"
+});
+});
+
+app.post("/submit",(req, res) =>{
+    res.render("about.ejs", {
+  title: "About",
+  activePage: "About"
+});
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
